d038bb8233837bc0b45fb0031ecb4a7022e2ed7c
Nicolas Erramuspe <nicolas@minimo.io>
2024-11-15 20:04:26 -0300

    Adding flowbite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,37 @@
 <script lang="ts">
+    import {
+        Button,
+        Alert,
+        Card,
+        Navbar,
+        NavBrand,
+        NavLi,
+        NavUl,
+        NavHamburger,
+        Toast,
+    } from "flowbite-svelte";
 </script>
 
 <svelte:head>
     <title>Início</title>
     <meta name="description" content="Seja um ninja das redes neuronais" />
 </svelte:head>
+
+<div>
+    <Button color="primary" class="my-4">Primary</Button>
+    <Alert color="primary" class="my-4">Primary</Alert>
+    <Card href="/cards" color="primary" class="my-4">
+        <h5
+            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-red-800"
+        >
+            Noteworthy technology acquisitions 2021
+        </h5>
+        <p class="font-normal text-gray-700 dark:text-gray-700 leading-tight">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+        </p>
+    </Card>
+</div>
 
 <section>
     <h1>NeuralNinja</h1>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,42 @@
-import aspectRatio from '@tailwindcss/aspect-ratio';
-import forms from '@tailwindcss/forms';
-import type { Config } from 'tailwindcss';
+import aspectRatio from "@tailwindcss/aspect-ratio";
+import forms from "@tailwindcss/forms";
+import type { Config } from "tailwindcss";
+import flowbitePlugin from "flowbite/plugin";
+
+// export default {
+//     content: ["./src/**/*.{html,js,svelte,ts}"],
+
+//     theme: {
+//         extend: {},
+//     },
+
+//     plugins: [forms, aspectRatio],
+// } satisfies Config;
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-
-  theme: {
-    extend: {}
-  },
-
-  plugins: [forms, aspectRatio]
-} satisfies Config;
+    content: [
+        "./src/**/*.{html,js,svelte,ts}",
+        "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+    ],
+    darkMode: "selector",
+    theme: {
+        extend: {
+            colors: {
+                // flowbite-svelte
+                primary: {
+                    50: "#FFF5F2",
+                    100: "#FFF1EE",
+                    200: "#FFE4DE",
+                    300: "#FFD5CC",
+                    400: "#FFBCAD",
+                    500: "#FE795D",
+                    600: "#EF562F",
+                    700: "#EB4F27",
+                    800: "#CC4522",
+                    900: "#A5371B",
+                },
+            },
+        },
+    },
+    plugins: [forms, aspectRatio, flowbitePlugin],
+} as Config;
